170f863d02c27d0ff0235c2d2889d8e3f02358f6
Matheus Assis <matheusassissilva20@gmail.com>
2021-12-18 23:28:07 -0300

    Create One-To-Many relationship with Episode model

--- src/main/java/com/example/podlib/domain/model/Author.java
+++ src/main/java/com/example/podlib/domain/model/Author.java
@@ -2,10 +2,18 @@ package com.example.podlib.domain.model;
 
 import lombok.Data;
 
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class Author {
     private String name;
     private String description;
     private String category;
+
+    @OneToMany
+    private List<Episode> episodes = new ArrayList<>();
 
 }
