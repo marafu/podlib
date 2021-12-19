ae7ac2e1d7cfbf990cbec1b4fc3dd939f0781e4c
Matheus Assis <matheusassissilva20@gmail.com>
2021-12-18 23:27:23 -0300

    Create Many-To-One relationship with Author model

--- src/main/java/com/example/podlib/domain/model/Episode.java
+++ src/main/java/com/example/podlib/domain/model/Episode.java
@@ -2,13 +2,21 @@ package com.example.podlib.domain.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 public class Episode {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String title;
     private String description;
     private Long duration;
     private String path;
     private String thumbnail;
+
+    @ManyToOne
     private Author author;
 
 }
